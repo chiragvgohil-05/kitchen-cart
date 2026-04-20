029268475497b57eb8a0f06940e66512d741c43f
chiragvgohil-05 <chiragvgohil05@gmail.com>
2026-04-20 07:56:16 +0530

    minor

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://noncleistogamic-unridiculously-adonis.ngrok-free.dev/api',
+    baseURL: 'https://headed-body-attest.ngrok-free.dev/api',
     headers: {
         'Content-Type': 'application/json',
     },
